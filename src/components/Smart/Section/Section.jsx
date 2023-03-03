f2dc108cc00860f7b4e1b50c602f2d493b8b068f
MacBook <21mechanic21@gmail.com>
2023-03-03 15:07:43 +0200

    create modal windows

--- src/components/Smart/Section/Section.jsx
+++ src/components/Smart/Section/Section.jsx
@@ -155,9 +155,109 @@ export const Section = () => {
                 <div className={s.week_weather_modal_window}>
 
                     <div className={s.modal_window_col}>
-                        
+
+                        <table>
+
+                            <tr className={s.tabel_title_row}>
+
+                                <th>Day</th>
+
+                                <th></th>
+
+                                <th>Min</th>
+
+                                <th>Max</th>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Tuesday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Wednesday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Thursday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Friday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Saturday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                            <tr className={s.tabel_day_forecast}>
+
+                                <td>Sunday</td>
+
+                                <td className={s.tabel_day_forecas_img}>
+                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+                                </td>
+
+                                <td>-10°</td>
+
+                                <td>-15°</td>
+
+                            </tr>
+
+                        </table>
+
                     </div>
-                        
+
                 </div>
 
             </div>
