7c1f5e9fe71200ec75b98e6cb14ff35272518b3a
MacBook <21mechanic21@gmail.com>
2023-03-19 15:32:41 +0200

    finish feature

---
+++ src/components/Ordinary/Daily_forecast/Daile_forecast.jsx
@@ -0,0 +1,97 @@
+import s from './Daile_forecast.module.scss';
+
+export const Daile_forecast = () => {
+
+    return (
+
+        <div className={s.weather_day_row}>
+
+            <div className={s.weather_day_col}>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+                <div className={s.item}>
+
+                    <p>11 Am</p>
+
+                    <img src="/img/weather_ico/Rainy.svg" alt="" />
+
+                    <h3>-10°</h3>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+
+}
--- src/components/Smart/Section/Section.jsx
+++ src/components/Smart/Section/Section.jsx
@@ -1,6 +1,12 @@
-import s from './Section.module.scss'
+import React, { useState, useEffect } from 'react';
+import s from './Section.module.scss';
+import { Daile_forecast } from '../../Ordinary/Daily_forecast/Daile_forecast';
+import {Weekly_forecast} from '../../Ordinary/Weekly_forecast/Weekly_forecast';
 
 export const Section = () => {
+
+    const [bottom_sheet_open, set_bottom_sheet_open] = useState(false)
+
 
     return (
 
@@ -64,201 +70,9 @@ export const Section = () => {
 
                 </div>
 
-                <div className={s.weather_day_row}>
+                <Daile_forecast/>
 
-                    <div className={s.weather_day_col}>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                        <div className={s.item}>
-
-                            <p>11 Am</p>
-
-                            <img src="/img/weather_ico/Rainy.svg" alt="" />
-
-                            <h3>-10°</h3>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div className={s.week_weather_modal_window}>
-
-                    <div className={s.modal_window_col}>
-
-                        <table>
-
-                            <tr className={s.tabel_title_row}>
-
-                                <th>Day</th>
-
-                                <th></th>
-
-                                <th>Min</th>
-
-                                <th>Max</th>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Tuesday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Wednesday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Thursday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Friday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Saturday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                            <tr className={s.tabel_day_forecast}>
-
-                                <td>Sunday</td>
-
-                                <td className={s.tabel_day_forecas_img}>
-                                    <img src="/img/weather_ico/Rainy.svg" alt="" />
-                                </td>
-
-                                <td>-10°</td>
-
-                                <td>-15°</td>
-
-                            </tr>
-
-                        </table>
-
-                    </div>
-
-                </div>
+                <Weekly_forecast/>
 
             </div>
 
